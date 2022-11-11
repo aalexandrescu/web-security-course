b06200b31ea7a3b9b0c40fd33954079892162de6
adrian-alexandrescu <adrian.alexandrescu@academic.tuiasi.ro>
2022-11-11 18:26:44 +0200

    work in progress @ adding a user in the db

---
+++ target-server/db_connection.py
@@ -0,0 +1,28 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print("SQLLITE3 version %s" % sqlite3.version)
+    except Error as e:
+        print(e)    
+    return conn
+
+def close_connection(conn):
+    if conn:
+        conn.close()
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
---
+++ target-server/db_init.py
@@ -0,0 +1,19 @@
+import db_connection
+
+def init_db():
+    database = ":memory:"
+
+    sql_create_table_users = """ CREATE TABLE IF NOT EXISTS users (
+                                        id integer PRIMARY KEY,
+                                        email text NOT NULL                                        
+                                ); """
+    
+    conn = db_connection.create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create users table
+        db_connection.create_table(conn, sql_create_table_users)
+
+    else:
+        print("Error! cannot create the database connection.")
